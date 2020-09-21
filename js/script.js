6501ad93f53eecb16e11baa8f0d16fe7968ccd8e
kgbsenior <Rostik3014@gmail.com>
2020-09-21 14:21:08 +0300

    backup

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ var heroes_blocks = heroes_container.querySelectorAll(".block")
 let selectedblocks = {}
 let cart = document.getElementById("cart")
 let blockcounter = 0
+let heroesplace = document.getElementById("heroes-place")
 
-selectedblocks.length = heroes_blocks.length-1
-for(let i = 0; i<selectedblocks.length-1;++i){
+selectedblocks.length = heroes_blocks.length
+for(let i = 0; i<selectedblocks.length;++i){
     selectedblocks[i] = 0
 }
 
@@ -13,23 +14,98 @@ for(let block of heroes_blocks){
     block.addEventListener("click", (elem)=>{
         console.log(block.dataset.hero)
         let checkbox = block.querySelector("input")
+        
         cart.classList.add("active-cart")
         if(!checkbox.checked){
             checkbox.checked = true
             selectedblocks[block.dataset.hero] = 1
             console.log(checkbox.checked)
-            cart.querySelector("p").innerHTML=selectedblocks
+          /*  cart.querySelector("p").innerHTML=""
+            for(let i = 0;i<selectedblocks.length; ++i){
+                cart.querySelector("p").insertAdjacentHTML("beforeend", selectedblocks[i])
+                cart.querySelector("p").insertAdjacentHTML("beforeend", " ")
+             } */
             ++blockcounter
         }
         else{
             checkbox.checked = false
             selectedblocks[block.dataset.hero] = 0
             console.log(checkbox.checked)
-            cart.querySelector("p").innerHTML=selectedblocks
             --blockcounter
             if(blockcounter==0){
                 cart.classList.remove("active-cart")
             }
+            /* cart.querySelector("p").innerHTML=""
+              for(let i = 0;i<selectedblocks.length; ++i){
+                cart.querySelector("p").insertAdjacentHTML("beforeend", selectedblocks[i])
+                cart.querySelector("p").insertAdjacentHTML("beforeend", " ")
+            }
+            */
+        }
+        if(selectedblocks[0]==1){
+            if(!document.getElementById("hero1")){
+                heroesplace.insertAdjacentHTML("beforeend", "<h2 id='hero1'>Asilak</h2>")
+            }
+        }
+        else{
+            if(document.getElementById("hero1")){
+                document.getElementById("hero1").remove()
+            } 
+        }
+
+        if(selectedblocks[1]==1){
+            if(!document.getElementById("hero2")){
+                heroesplace.insertAdjacentHTML("beforeend", "<h2 id='hero2'>Dzedka</h2>")
+            }
+        }
+        else{
+            if(document.getElementById("hero2")){
+                document.getElementById("hero2").remove()
+            } 
+        }
+
+        if(selectedblocks[2]==1){
+            if(!document.getElementById("hero3")){
+                heroesplace.insertAdjacentHTML("beforeend", "<h2 id='hero3'>Balamuten</h2>")
+            }
+        }
+        else{
+            if(document.getElementById("hero3")){
+                document.getElementById("hero3").remove()
+            } 
+        }
+
+        if(selectedblocks[3]==1){
+            if(!document.getElementById("hero4")){
+                heroesplace.insertAdjacentHTML("beforeend", "<h2 id='hero4'>Fish Tsar</h2>")
+            }
+        }
+        else{
+            if(document.getElementById("hero4")){
+                document.getElementById("hero4").remove()
+            } 
+        }
+
+        if(selectedblocks[4]==1){
+            if(!document.getElementById("hero5")){
+                heroesplace.insertAdjacentHTML("beforeend", "<h2 id='hero5'>Zhiten</h2>")
+            }
+        }
+        else{
+            if(document.getElementById("hero5")){
+                document.getElementById("hero5").remove()
+            } 
+        }
+
+        if(selectedblocks[5]==1){
+            if(!document.getElementById("hero6")){
+                heroesplace.insertAdjacentHTML("beforeend", "<h2 id='hero6'>Hatnik</h2>")
+            }
+        }
+        else{
+            if(document.getElementById("hero6")){
+                document.getElementById("hero6").remove()
+            } 
         }
     })
 }
